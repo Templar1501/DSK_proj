1996acbc1a37353bf955861a43558b7d227bbb25
0xQQa <kacperk799@gmail.com>
2021-03-24 11:42:05 +0100

    Changes to be committed: 	modified:   ClientApplication/P2PUser.c 	new file:   ClientApplication/P2PUser.h

---
+++ ClientApplication/P2PUser.c
@@ -0,0 +1,30 @@
+#include "P2PUser.h"
+
+//Odbieranie danych
+void rx() {
+
+}
+
+//Wysyłanie danych
+void tx() {
+
+}
+
+//Obsługa informacji socketu
+void handle_rxtx() {
+
+}
+
+//Stworzenie socketu dla nasłuchiwania połączenia
+void set_out_data_socket() {
+
+}
+
+//Stworzenie socketu do nawiązywania połączenia
+void set_in_data_socket() {
+
+}
+
+int main(int argc, char ** argv) {
+    return 0;
+}
---
+++ ClientApplication/P2PUser.h
@@ -0,0 +1,4 @@
+typedef struct client_properies {
+    unsigned long   cash_amount;
+    long            trust_level;
+} P2PUser;
