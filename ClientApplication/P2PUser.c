19ac7aef741b89bb9f55b238ad763e401b9e1fd1
0xQQa <kacperk799@gmail.com>
2021-03-24 12:01:45 +0100

    Changes to be committed: 	modified:   P2PUser.c 	modified:   P2PUser.h

--- ClientApplication/P2PUser.c
+++ ClientApplication/P2PUser.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
 #include "P2PUser.h"
 
 //Odbieranie danych
@@ -25,6 +30,29 @@ void set_in_data_socket() {
 
 }
 
-int main(int argc, char ** argv) {
+void parse_args(int argc, char **argv, P2PUser *user_properties) {
+    size_t user_name_len;
+
+    if (argc != 2) {
+        printf("Usage: %s <name>\n", argv[0]);
+        exit(-1);
+    }
+
+    user_name_len = strlen(argv[1]);
+    printf("%d\n", user_name_len);
+    if (user_name_len > USER_NAME_LEN - 1) {
+        printf("Usage: %s <name>\n", argv[0]);
+        exit(-1);
+    }
+
+    strncpy(user_properties->name, argv[1], user_name_len - 1);
+    user_properties->name[user_name_len - 1] = '\0';
+}
+
+int main(int argc, char **argv) {
+    P2PUser user_properties;
+
+    parse_args(argc, argv, &user_properties);
+
     return 0;
 }
--- ClientApplication/P2PUser.h
+++ ClientApplication/P2PUser.h
@@ -1,4 +1,7 @@
+#define USER_NAME_LEN 10
+
 typedef struct client_properies {
     unsigned long   cash_amount;
     long            trust_level;
+    char            name[USER_NAME_LEN];
 } P2PUser;
